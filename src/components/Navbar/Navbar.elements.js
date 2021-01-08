3f233278f1af5df77d0653e17c32338a0e88ac7d
Patryk Orłowski <orzeleagle122@gmail.com>
2021-01-08 19:44:18 +0100

    create Navbar

---
+++ src/components/Navbar/Navbar.elements.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const StyledNav=styled.nav`
+    background: #101522;
+    height: 80px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.2rem;
+    position: sticky;
+    top:0;
+    z-index: 999;
+`;
+
+export const StyledNavbarContainer=styled(Container)`
+    display: flex;
+    justify-content: space-between;
+    height: 80px;
+
+    $(Container)
+`;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {StyledNav, StyledNavbarContainer} from './Navbar.elements'
+
+const Navbar = () => {
+    return ( 
+        <>
+            <StyledNav>
+                <StyledNavbarContainer>
+
+                </StyledNavbarContainer>
+
+            </StyledNav>
+        </>
+     );
+}
+ 
+export default Navbar;
